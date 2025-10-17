0a33ac4369014af0fa4892d3ed9c94eabf396a05
Simran Kaur <simrankaur47937@gmail.com>
2025-10-17 19:19:34 +0530

    editing deleting and templates rendering

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("posts/", views.post_list, name="post_list"),
+    path("create/", views.post_create, name="post_create"),
+    path("delete/", views.post_delete, name="post_delete"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -12,7 +12,33 @@ def post_list(request):
 
 def post_create(request):
     if request.method == 'POST':
-        pass
+        form =  PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'post_create.html', {'form': form})
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, user= request.user)
+    if request.method == 'POST':
+        form =  PostForm(request.POST, request.FILES, instance=Post)
+        if form.is_valid():
+                post = form.save(commit=False)
+                post.user = request.user
+                post.save()
+                return redirect('post_list')
+        
+    else:
+        post = PostForm(instance=Post)
+    return render(request, 'post_create.html', {'form': form})
+    
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, pk= post_id, user = request.user)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'post_delete_confirmation.html', {'post': post})
